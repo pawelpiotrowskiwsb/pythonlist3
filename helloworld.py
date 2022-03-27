93118f26e2efecc654ff04e5d5a7611a0f9379b3
pawelpiotrowskiwsb <91828950+pawelpiotrowskiwsb@users.noreply.github.com>
2022-03-27 13:10:54 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print ("hello world")
+from flask import flask
+app = flask(_hello_)
+
+(app.route('/hello/<hello>')
+ def hello_world(hello):
+  return 'Hello world'
+ 
+ if _hello_ == '_main_':
+  app.run()
